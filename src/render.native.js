bea707cdd58cb794564cb2f5fc7d232bd020bd32
Matt Styles <matt@personalurban.com>
2016-09-26 09:36:43 +0100

    add separate package meta for web and native

--- src/render.native.js
+++ src/render.native.js
@@ -1,11 +1,18 @@
 
+import React from 'react'
+import {
+  Text,
+  View
+} from 'react-native'
+
 import styles from './styles'
 import List from './list'
 
 export default function (props) {
   return (
     <View>
-      <Text>Hairy Native View</Text>
+      <Text>Hairy Native View!!</Text>
+      <List items={this.state.items} />
     </View>
   )
 }
